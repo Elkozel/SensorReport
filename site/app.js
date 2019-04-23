a5e7b56280ac2ad877641fe355005f7ba314f7a1
Tsvetomir Hristov <proo8@abv.bg>
2019-04-23 22:55:17 +0200

    fixing path to index

--- site/app.js
+++ site/app.js
@@ -10,7 +10,7 @@ var port = 80;
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + '/site/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 dispatcher.event.on("update", function (scope, msg) {
