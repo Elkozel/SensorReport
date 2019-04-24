f3f012c8566b1a89ccb363d453b68b3b70132fb0
Tsvetomir Hristov <proo8@abv.bg>
2019-04-24 16:05:23 +0200

    Adding IMU support

--- code/code.ino
+++ code/code.ino
@@ -9,16 +9,20 @@
 #include <BME280I2C_BRZO.h>
 #include <EnvironmentCalculations.h>
 
+// MCP9250-1.0.1 - Version: Latest
+#include <MPU9250.h>
+
 // I2C Definitions
 #define I2C_SD_Pin A4
 #define I2C_SK_Pin A5
-#define I2C_BMP280_Addr
+#define I2C_BMP280_Addr 0x76
 #define I2C_MCP9808_Addr 0x18
-#define I2C_IMU_Addr
+#define I2C_MPU9250_Addr 0x68
 
 // Declare Globals
 Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 BME280I2C barsensor;
+MPU9250 IMU(Wire, I2C_MPU9250_Addr);
 struct t_report{
     uint8_t BMP280_flag = 0;
     float BMP280_temp = 0;
@@ -27,9 +31,15 @@ struct t_report{
     uint8_t MCP9808_flag = 0;
     float MCP9808_temp = 0;
     uint16_t MCP9808_samples = 0;
+    uint8_t MPU9250_flag = 0;
+    float MPU9250_accel[3];
+    float MPU9250_gyro[3];
+    float MPU9250_mag[3];
+    float MPU9250_temp = 0;
+    uint16_t MPU9250_samples = 0;
 };
 
-void barRoutine(unsigned long), tempRoutine(unsigned long), setTempResolution(int), submitReport(), reportRoutine();
+void barRoutine(unsigned long), tempRoutine(unsigned long), IMURoutine(unsigned long), setTempResolution(int), submitReport(), reportRoutine();
 
 // MCP9808 Globals
 unsigned int MCP9808_Interval;
@@ -38,6 +48,10 @@ unsigned long MCP9808_Timestamp;
 // BME280 Globals
 unsigned int BMP280_Interval;
 unsigned long BMP280_Timestamp;
+
+// MPU9250 Globals
+unsigned int MPU9250_Interval;
+unsigned long MPU9250_Timestamp;
 
 // Global Flags
 unsigned char tempFlag = 0;
@@ -70,17 +84,36 @@ void setup() {
   BMP280_Interval = 50;
   BMP280_Timestamp = millis();
   Serial.println(F("Barometric sensor set successfully"));
+
+  Serial.println(F("Setting IMU"));
+  int status = 0;
+  if((status = IMU.begin())<0){
+    Serial.print(F("MPU9250 Setup failed (Status: "));
+    Serial.print(status);
+    Serial.println(F(")"));
+    while(1);
+  }
+  IMU.setAccelRange(MPU9250::ACCEL_RANGE_8G);
+  IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
+  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
+  IMU.setSrd(19);
+  Serial.println(F("IMU sensor set successfully"));
+  BMP280_Interval = 20;
+  BMP280_Timestamp = millis();
+
+  Serial.println(F("Setup complete"));
 }
 
 void loop() {
   unsigned long currentMillis = millis();
   tempRoutine(currentMillis);
   barRoutine(currentMillis);
+  IMURoutine(currentMillis);
   reportRoutine();
 }
 
 void reportRoutine() {
-  if(c_report.BMP280_flag == 1 && c_report.MCP9808_flag == 1){
+  if(c_report.BMP280_flag == 1 && c_report.MCP9808_flag == 1 && c_report.MPU9250_flag == 1){
     submitReport();
   }
 }
@@ -89,28 +122,26 @@ void submitReport() {
   p_report = c_report;
   t_report n_report;
   c_report = n_report;
+  
   Serial.print((char) 1); // Begin transmission
-  Serial.print((char) 0); // Status of message
-  char *temp = (char *) &p_report.BMP280_temp; // Warning, spagetti code coming up!
-  for(int s=0; s<4; s++){
-    Serial.print(*(temp+s));
-  }
-  temp = (char *) &p_report.BMP280_pres;
-  for(int s=0; s<4; s++){
-    Serial.print(*(temp+s));
-  }
-  temp = (char *) &p_report.BMP280_samples;
-  for(int s=0; s<2; s++){
-    Serial.print(*(temp+s));
-  }
-  temp = (char *) &p_report.MCP9808_temp;
-  for(int s=0; s<4; s++){
-    Serial.print(*(temp+s));
-  }
-  temp = (char *) &p_report.MCP9808_samples;
-  for(int s=0; s<2; s++){
-    Serial.print(*(temp+s));
-  }
+  Serial.print((char) 1); // Set sensor BMP280/MCP9808
+  Serial.write((char *)&p_report.BMP280_temp, 4);
+  Serial.write((char *)&p_report.BMP280_pres, 4);
+  Serial.write((char *)&p_report.BMP280_samples, 2);
+  Serial.write((char *)&p_report.MCP9808_temp, 4);
+  Serial.write((char *)&p_report.MCP9808_samples, 2);
+  Serial.write((char *)&p_report.MPU9250_accel, 12);
+  Serial.write((char *)&p_report.MPU9250_gyro, 12);
+  Serial.write((char *)&p_report.MPU9250_mag, 12);
+  Serial.write((char *)&p_report.MPU9250_temp, 4);
+  Serial.write((char *)&p_report.MPU9250_samples, 2);
+  Serial.print((char) 4); // End transmission
+}
+
+void statusMsg(const char * msg){
+  Serial.print((char) 1); // Begin transmission
+  Serial.print((char) 0); // Set status
+  Serial.print(msg);
   Serial.print((char) 4); // End transmission
 }
 
@@ -149,6 +180,50 @@ void tempRoutine(unsigned long clock) {
       c_report.MCP9808_flag = 1; // Raise temperature flag
     }
     MCP9808_Timestamp = clock + MCP9808_Interval;
+  }
+}
+
+void IMURoutine(unsigned long clock) {
+  if (MPU9250_Timestamp < clock){
+    IMU.readSensor();
+    if(c_report.MPU9250_flag > 0){
+      c_report.MPU9250_accel[0] += IMU.getAccelX_mss();
+      c_report.MPU9250_accel[0] /= 2;
+      c_report.MPU9250_accel[1] += IMU.getAccelY_mss();
+      c_report.MPU9250_accel[1] /= 2;
+      c_report.MPU9250_accel[2] += IMU.getAccelZ_mss();
+      c_report.MPU9250_accel[2] /= 2;
+      c_report.MPU9250_gyro[0] += IMU.getGyroX_rads();
+      c_report.MPU9250_gyro[0] /= 2;
+      c_report.MPU9250_gyro[1] += IMU.getGyroY_rads();
+      c_report.MPU9250_gyro[1] /= 2;
+      c_report.MPU9250_gyro[2] += IMU.getGyroZ_rads();
+      c_report.MPU9250_gyro[2] /= 2;
+      c_report.MPU9250_mag[0] += IMU.getMagX_uT();
+      c_report.MPU9250_mag[0] /= 2;
+      c_report.MPU9250_mag[1] += IMU.getMagY_uT();
+      c_report.MPU9250_mag[1] /= 2;
+      c_report.MPU9250_mag[2] += IMU.getMagZ_uT();
+      c_report.MPU9250_mag[2] /= 2;
+      c_report.MPU9250_temp += IMU.getTemperature_C();
+      c_report.MPU9250_temp /= 2;
+      c_report.MPU9250_samples++;
+    }
+    else{
+      c_report.MPU9250_accel[0] = IMU.getAccelX_mss();
+      c_report.MPU9250_accel[1] = IMU.getAccelY_mss();
+      c_report.MPU9250_accel[2] = IMU.getAccelZ_mss();
+      c_report.MPU9250_gyro[0] = IMU.getGyroX_rads();
+      c_report.MPU9250_gyro[1] = IMU.getGyroY_rads();
+      c_report.MPU9250_gyro[2] = IMU.getGyroZ_rads();
+      c_report.MPU9250_mag[0] = IMU.getMagX_uT();
+      c_report.MPU9250_mag[1] = IMU.getMagY_uT();
+      c_report.MPU9250_mag[2] = IMU.getMagZ_uT();
+      c_report.MPU9250_temp = IMU.getTemperature_C();
+      c_report.MPU9250_flag = 1;
+      c_report.MPU9250_samples++;
+    }
+    MPU9250_Timestamp = clock + MPU9250_Interval;
   }
 }
 
